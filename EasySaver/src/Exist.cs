a91ab2d06f9f206b75545072251efb19c63700af
Muhammed Enes Okullu <meokullu@gmail.com>
2024-03-20 19:47:59 +0300

    v1.3.0

--- EasySaver/src/Exist.cs
+++ EasySaver/src/Exist.cs
@@ -7,7 +7,7 @@ namespace EasySaver.Common
         #region Existing
 
         /// <summary>
-        /// Check if file existing via <see cref="System.IO.File.Exists(string?)"/>.
+        /// Check if file existing via <see cref="System.IO.File.Exists(string)"/>.
         /// </summary>
         /// <param name="path">Path to check if exists.</param>
         /// <returns>Returns true if the file does exist, returns false if the file doesn't exist.</returns>
@@ -27,7 +27,7 @@ namespace EasySaver.Common
         }
 
         /// <summary>
-        /// Check if folder is existing via <see cref="System.IO.Directory.Exists(string?)"/>. 
+        /// Check if folder is existing via <see cref="System.IO.Directory.Exists(string)"/>. 
         /// </summary>
         /// <param name="path">Path to check if exists.</param>
         /// <returns>Returns true if the folder does exist, returns false if the folder doesn't exist.</returns>
